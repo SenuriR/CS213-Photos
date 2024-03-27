2991c6992e91bc7fd96fa0cf507a0b6ab71b1603
ishamkhan23 <109629147+ishamkhan23@users.noreply.github.com>
2024-03-27 18:03:01 -0400

    Update Album.java

--- src/model/Album.java
+++ src/model/Album.java
@@ -2,33 +2,70 @@ package model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class Album implements Serializable {
-    private String name;
-    private List<Photo> photos;
 
-    public Album(String name) {
-        this.name = name;
-        this.photos = new ArrayList<>();
-    }
-
-    // Getters and Setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Photo> getPhotos() {
-        return photos;
-    }
-
-    public void addPhoto(Photo photo) {
-        this.photos.add(photo);
-    }
-
-    // Similarly, methods to remove photos, etc., can be added.
+	private static final long serialVersionUID = 1891567810783724951L;
+	private String name;
+	private ArrayList<Photo> photos;
+	
+	/**
+	 * Constructor
+	 * @param name
+	 */
+	public Album(String name) {
+		this.name = name;
+		photos = new ArrayList<Photo>();
+	}
+	
+	/**
+	 * Gets the name of this album
+	 * @return the name of this album
+	 */
+	public String getName() {
+		return this.name;
+	}
+	
+	/**
+	 * Sets the name of this album
+	 * @param name the new name of this album
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Gets the photos in this album
+	 * @return an arraylist of photos
+	 */
+	public ArrayList<Photo> getPhotos() {
+		return this.photos;
+	}
+	
+	/**
+	 * Returns the number of photos in this album
+	 * @return the number of photos in this album
+	 */
+	public int getPhotoCount() {
+		return this.photos.size();
+	}
+	
+	/**
+	 * Compares this album to another
+	 * @param other the album to be compared
+	 * @return true if the albums are equal, false otherwise
+	 */
+	public boolean equals(Album other) {
+		return name.equals(other.name);
+	}
+	
+	/**
+	 * Returns a string representation of this album
+	 */
+	public String toString() {
+		String result = "NAME: " + name + "\nPHOTO COUNT: " + photos.size(); 
+		
+		return result;
+	}
 }
